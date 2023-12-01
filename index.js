0ed3178bc4a09919c475e367fadaa0b8fcfe7e51
Jean <85579388+jeangoedert@users.noreply.github.com>
2023-12-01 12:54:50 +0000

    codigo

---
+++ index.js
@@ -0,0 +1,8 @@
+let heroi = [
+    { "nome": "Mestre dos Magos", "idade": "500", "tipo": "mago", "ataque": "magia"},
+    { "nome": "Conan", "idade": "28", "tipo": "guerreiro", "ataque": "espada"},
+    { "nome": "Senhor Mingau", "idade": "85", "tipo": "monge", "ataque": "artes marciais"},
+    { "nome": "Formiga Atômica", "idade": "5", "tipo": "ninja", "ataque": "shuriken"}
+];
+let heroiEscolhido = heroi[3];
+  console.log("O " + heroiEscolhido.tipo + " atacou usando " + heroiEscolhido.ataque + ".");
